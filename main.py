b83440634191c000b6f495fbdc91133ac2d75c62
5CYTH3 <ScytheD@outlook.fr>
2022-03-22 21:59:56 +0100

    Added basic UI

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import matplotlib.pyplot as mp
+import matplotlib as m
 import pandas as p
+
 
 def read_data():
     set = p.read_csv("dataset.csv")
     scores = set['score'].sort_values(ascending=True)
-    indexes = set['score'].sort_index(ascending=True)
-    print(scores)
+    mp.plot()
+    mp.show()
+
 
 read_data()
