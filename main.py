20f5f9ffaeabc9e0508f9455057ff57476aeceab
5CYTH3 <ScytheD@outlook.fr>
2022-03-22 09:50:07 +0100

    First filtering

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pandas as p
 
 def read_data():
     set = p.read_csv("dataset.csv")
-    print(len(set.columns))
-    print()
+    scores = set['score'].sort_values(ascending=True)
+    indexes = set['score'].sort_index(ascending=True)
+    print(scores)
 
 read_data()
