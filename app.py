9dab1044233f41d82da39c23458064fc9760b876
5CYTH3 <ScytheD@outlook.fr>
2022-03-24 20:37:30 +0100

    Added template inheritance

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
